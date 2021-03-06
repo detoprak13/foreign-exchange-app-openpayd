b35c5efb13770d66a42cf9a32f6fb975a6793bb9
Deniz Toprak <dtoprak@octovan.com>
2021-03-06 18:07:46 +0300

    unnecessary import removed

--- src/test/java/com/openpayd/foreignexchangeapp/ForeignTransactionAppApplicationTests.java
+++ src/test/java/com/openpayd/foreignexchangeapp/ForeignTransactionAppApplicationTests.java
@@ -13,7 +13,6 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 

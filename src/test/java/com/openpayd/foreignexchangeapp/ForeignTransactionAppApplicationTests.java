30b9fb0c33e877c63ee40cd1aa201ad8e98f3d9a
Deniz Toprak <dtoprak@octovan.com>
2021-03-06 15:49:08 +0300

    test & doc

--- src/test/java/com/openpayd/foreignexchangeapp/ForeignTransactionAppApplicationTests.java
+++ src/test/java/com/openpayd/foreignexchangeapp/ForeignTransactionAppApplicationTests.java
@@ -75,14 +75,121 @@ class ForeignTransactionAppApplicationTests {
 		MockHttpServletRequestBuilder builder = MockMvcRequestBuilders
 				.get("/exchanges")
 				.param("sortDirection", "DESC")
-				.param("sortDirection", "0")
+				.param("pageNo", "0")
 				.param("sortParam", "id")
 				.param("dateStart", "2021-03-02")
 				.param("dateEnd", "2021-03-02");
 
 		mockMvc.perform(builder)
 				.andExpect(status().isOk())
-				.andDo(MockMvcResultHandlers.print());
+				.andDo(document("all-transactions"));
 	}
 
+	@Test
+	void getAllTransactions_invalidDate() throws Exception {
+		MockHttpServletRequestBuilder builder = MockMvcRequestBuilders
+				.get("/exchanges")
+				.param("sortDirection", "DESC")
+				.param("pageNo", "0")
+				.param("sortParam", "id")
+				.param("dateStart", "2021-03-02")
+				.param("dateEnd", "2021-03-023");
+
+		mockMvc.perform(builder)
+				.andExpect(status().isBadRequest())
+				.andExpect(content().contentType(MediaType.APPLICATION_JSON))
+				.andExpect(jsonPath("$.exceptionType", is(ExceptionType.INVALID_DATE.name())))
+				.andDo(document("all-transactions-invalid-date"));
+	}
+
+	@Test
+	void getAllTransactions_invalidPageNo() throws Exception {
+		MockHttpServletRequestBuilder builder = MockMvcRequestBuilders
+				.get("/exchanges")
+				.param("sortDirection", "DESC")
+				.param("pageNo", "-1")
+				.param("sortParam", "id")
+				.param("dateStart", "2021-03-02")
+				.param("dateEnd", "2021-03-02");
+
+		mockMvc.perform(builder)
+				.andExpect(status().isBadRequest())
+				.andExpect(content().contentType(MediaType.APPLICATION_JSON))
+				.andExpect(jsonPath("$.exceptionType", is(ExceptionType.INVALID_PAGE_NO.name())))
+				.andDo(document("all-transactions-invalid-pageNo"));
+	}
+
+	@Test
+	void getAllTransactions_invalidSortDirection() throws Exception {
+		MockHttpServletRequestBuilder builder = MockMvcRequestBuilders
+				.get("/exchanges")
+				.param("sortDirection", "DESCA")
+				.param("pageNo", "0")
+				.param("sortParam", "id")
+				.param("dateStart", "2021-03-02")
+				.param("dateEnd", "2021-03-02");
+
+		mockMvc.perform(builder)
+				.andExpect(status().isBadRequest())
+				.andExpect(content().contentType(MediaType.APPLICATION_JSON))
+				.andExpect(jsonPath("$.exceptionType", is(ExceptionType.INVALID_SORT_DIRECTION.name())))
+				.andDo(document("all-transactions-invalid-sort-direction"));
+	}
+
+	@Test
+	void getAllTransactions_invalidSortParam() throws Exception {
+		MockHttpServletRequestBuilder builder = MockMvcRequestBuilders
+				.get("/exchanges")
+				.param("sortDirection", "DESC")
+				.param("pageNo", "0")
+				.param("sortParam", "ida")
+				.param("dateStart", "2021-03-02")
+				.param("dateEnd", "2021-03-02");
+
+		mockMvc.perform(builder)
+				.andExpect(status().isBadRequest())
+				.andExpect(content().contentType(MediaType.APPLICATION_JSON))
+				.andExpect(jsonPath("$.exceptionType", is(ExceptionType.INVALID_SORT_PARAM.name())))
+				.andDo(document("all-transactions-invalid-sort-param"));
+	}
+
+	@Test
+	void exchange_valid() throws Exception {
+		MockHttpServletRequestBuilder builder = MockMvcRequestBuilders
+				.post("/exchanges")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content("{ \"sourceCurrencyCode\": \"TRY\", \"targetCurrencyCode\": \"EUR\", \"amount\": \"100\" }") ;
+
+		mockMvc.perform(builder)
+				.andExpect(status().isOk())
+				.andDo(document("exchange_valid"));
+	}
+
+	@Test
+	void exchange_invalidCurrency() throws Exception {
+		MockHttpServletRequestBuilder builder = MockMvcRequestBuilders
+				.post("/exchanges")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content("{ \"sourceCurrencyCode\": \"TRYO\", \"targetCurrencyCode\": \"EUR\", \"amount\": \"100\" }") ;
+
+		mockMvc.perform(builder)
+				.andExpect(status().isBadRequest())
+				.andExpect(content().contentType(MediaType.APPLICATION_JSON))
+				.andExpect(jsonPath("$.exceptionType", is(ExceptionType.INVALID_CURRENCY.name())))
+				.andDo(document("exchange_invalid_currency"));
+	}
+
+	@Test
+	void exchange_invalidAmount() throws Exception {
+		MockHttpServletRequestBuilder builder = MockMvcRequestBuilders
+				.post("/exchanges")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content("{ \"sourceCurrencyCode\": \"TRY\", \"targetCurrencyCode\": \"EUR\", \"amount\": \"-5\" }") ;
+
+		mockMvc.perform(builder)
+				.andExpect(status().isBadRequest())
+				.andExpect(content().contentType(MediaType.APPLICATION_JSON))
+				.andExpect(jsonPath("$.exceptionType", is(ExceptionType.INVALID_EXCHANGE_AMOUNT.name())))
+				.andDo(document("exchange_invalid_amount"));
+	}
 }
